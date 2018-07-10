52515bd4e990895276753902297f0d86455df4b2
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-10 17:37:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,8 +40,8 @@ def over?(board)
   end
 end
 
-def winner(board)
+def winner?(board)
   if winning_combo = won?(board)
-    board[winning_combo.first]
+    return board[winning_combo.first]
   end
 end
